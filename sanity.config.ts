007df8a4b53ddb92100441a0d672b94cb251d7ab
Odd Egil Hegge Selnes <post@odde.org>
2025-08-31 09:46:03 +0200

    refactored minor details

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,15 +3,16 @@ import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
+// Environment variables for project configuration
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID || 'your-projectID'
+const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
+
 export default defineConfig({
   name: 'default',
-  title: 'kulturkompasset-backend',
-
-  projectId: '7jx6egsg',
-  dataset: 'production',
-
+  title: 'Kulturkompasset',
+  projectId,
+  dataset,
   plugins: [structureTool(), visionTool()],
-
   schema: {
     types: schemaTypes,
   },
